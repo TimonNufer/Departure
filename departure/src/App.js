678ec0a5a1a1cec7dae7351d01127eb26f2d4156
Nufer Timon (IT-PTR-CEN2-YPT - Extern) <timon.nufer@sbb.ch>
2023-05-05 16:04:47 +0200

    Added Login Page and started Router implementation

--- departure/src/App.js
+++ departure/src/App.js
@@ -1,23 +1,16 @@
 import Login from './Login';
+import HomePage from './HomePage'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Login />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Login />} />
+        <Route path='home' element={<HomePage />}/>
+      </Routes>
+    </BrowserRouter>
+    
   );
 }
 
--- departure/src/Login.js
+++ departure/src/Login.js
@@ -1,6 +1,52 @@
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import axios from 'axios';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  }
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    Validate(email, password);
+  }
+
+  const Validate = (email, password) => {
+    axios.post('http://localhost:4242/api/login', { email, password })
+      .then(response => {
+        sessionStorage.setItem('token', response.data.token);
+      }) 
+      .catch(error => {
+        throw new Error(error.response.data.message);
+      });
+  }
+
   return (
-    <h1>Test</h1>
+    <Form onSubmit={handleSubmit}>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" onChange={handleEmailChange} />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" onChange={handlePasswordChange} />
+      </Form.Group>
+
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
   );
 }
 
